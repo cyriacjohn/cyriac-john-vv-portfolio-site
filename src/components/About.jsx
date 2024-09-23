2eadf51b373e5fff8d303f2d5fc03f8ee06510d0
Cyriac John <76932582+cyriacjohn@users.noreply.github.com>
2024-09-23 13:36:46 +0530

    Update About.jsx
    changed the course details

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
                 <p className='text-5xl font-bold inline border-b-4 border-gray-500'>About</p>
             </div>
             <p className='text-xl mt-16'>
-            I hold a Master's degree in Computer Science from Central University of Kerala, equipping me with strong skills in HTML, CSS, JavaScript, and React for building dynamic websites. I'm also a proficient designer, using Photoshop to create visuals like brochures, logos, posters, and templates, which adds a creative touch to my work.
+            I hold a BTECH degree in Computer Science from Central University of Kerala, equipping me with strong skills in HTML, CSS, JavaScript, and React for building dynamic websites. I'm also a proficient designer, using Photoshop to create visuals like brochures, logos, posters, and templates, which adds a creative touch to my work.
             </p>
             <br />
             <p className='text-xl'>I'm an active contributor in various communities, fostering collaboration and insightful discussions. Notably, I collaborated with Kerala Startup Mission (KSUM) on a project, designing badges, ID cards, posters, and banners that beautifully blend technology and design.</p>
